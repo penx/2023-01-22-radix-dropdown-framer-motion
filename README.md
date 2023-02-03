b72e37c8a2e0345451962e373df70ea575563ff6
Alasdair McLeay <alasdair.mcleay@me.com>
2023-02-03 15:20:55 +0000

    no framer or tailwind

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# Animating a Radix Dropdown with Framer Motion
+# Animating a Radix Dropdown with CSS Animation
 
-Source code of the demo from my YouTube video
-
-- [Watch on YouTube](https://www.youtube.com/watch?v=p8CsotWUas0)
-- [See the diff from the video](https://github.com/samselikoff/2023-01-22-radix-dropdown-framer-motion/commit/8d10b1bf064cab82845d5c2678e9bc4e3ff9a715)
+Adapted from [Sam Selikoff's Framer Motion version](https://github.com/samselikoff/2023-01-22-radix-dropdown-framer-motion)
 
 ## Running the demo locally
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,123 +1,84 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { AnimatePresence, motion, useAnimationControls } from "framer-motion";
-import { ReactNode, useEffect, useState } from "react";
+import { useState, useRef } from "react";
+import "./styles.css";
 
 export default function App() {
-  let [text, setText] = useState("Select an item");
-  let [open, setOpen] = useState(false);
-  let controls = useAnimationControls();
+  const [selected, setSelected] = useState<number | null>(null);
+  const [open, setOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
 
-  async function closeMenu() {
-    await controls.start("closed");
+  function handleSelect(item: number) {
+    setSelected(item);
     setOpen(false);
   }
 
-  useEffect(() => {
-    if (open) {
-      controls.start("open");
-    }
-  }, [controls, open]);
-
   return (
-    <div className="flex min-h-full items-center justify-center">
-      <div className="mx-auto w-full max-w-sm overflow-hidden rounded-md border border-gray-300 bg-white">
-        <header className="border-b border-gray-100 p-2">
+    <div className="canvas">
+      <div className="panel">
+        <header className="header">
           <DropdownMenu.Root open={open} onOpenChange={setOpen}>
-            <DropdownMenu.Trigger className="cursor-default select-none rounded px-4 text-2xl hover:bg-gray-200/50 focus-visible:outline-none data-[state=open]:bg-gray-200/75">
+            <DropdownMenu.Trigger className="dropdown__trigger">
               
             </DropdownMenu.Trigger>
-
-            <AnimatePresence>
-              {open && (
-                <DropdownMenu.Portal forceMount>
+            <>
+              <>
+                <DropdownMenu.Portal>
                   <DropdownMenu.Content
                     align="start"
-                    className="mt-1 overflow-hidden rounded bg-white/75 p-2 text-left shadow backdrop-blur"
-                    asChild
+                    ref={contentRef}
+                    className="dropdown__content"
                   >
-                    <motion.div
-                      initial="closed"
-                      animate={controls}
-                      exit="closed"
-                      variants={{
-                        open: {
-                          opacity: 1,
-                          transition: { ease: "easeOut", duration: 0.1 },
-                        },
-                        closed: {
-                          opacity: 0,
-                          transition: { ease: "easeIn", duration: 0.2 },
-                        },
-                      }}
-                    >
+                    <>
+                      <Item onSelect={() => handleSelect(1)}>Item 1</Item>
+                      <Item onSelect={() => handleSelect(2)}>Item 2</Item>
                       <Item
-                        closeMenu={closeMenu}
-                        onSelect={() => setText("Clicked Item 1")}
+                        onSelect={() => {
+                          handleSelect(3);
+                          contentRef.current?.addEventListener(
+                            "animationend",
+                            () => alert(";)"),
+                            { once: true }
+                          );
+                        }}
                       >
-                        Item 1
-                      </Item>
-                      <Item
-                        closeMenu={closeMenu}
-                        onSelect={() => setText("Clicked Item 2")}
-                      >
-                        Item 2
-                      </Item>
-                      <Item closeMenu={closeMenu} onSelect={() => alert(";)")}>
                         Item 3
                       </Item>
-                    </motion.div>
+                    </>
                   </DropdownMenu.Content>
                 </DropdownMenu.Portal>
-              )}
-            </AnimatePresence>
+              </>
+            </>
           </DropdownMenu.Root>
         </header>
-        <div className="px-6 py-8 text-right">
-          <p>{text}</p>
+        <div className="message">
+          <p>{selected ? "Clicked item " + selected : "Select an item"}</p>
         </div>
       </div>
     </div>
   );
 }
 
-function Item({
-  children,
-  onSelect = () => {},
-  closeMenu,
-}: {
-  children: ReactNode;
-  onSelect?: () => void;
-  closeMenu: () => void;
-}) {
-  let controls = useAnimationControls();
-
+const Item = (
+  props: DropdownMenu.DropdownMenuItemProps & { onSelect?: () => void }
+) => {
   return (
     <DropdownMenu.Item
-      onSelect={async (e) => {
-        e.preventDefault();
-
-        await controls.start({
-          backgroundColor: "#fff",
-          color: "#000",
-          transition: { duration: 0.04 },
-        });
-        await controls.start({
-          backgroundColor: "#38bdf8",
-          color: "#fff",
-          transition: { duration: 0.04 },
-        });
-        await sleep(0.075);
-
-        await closeMenu();
-        onSelect();
+      {...props}
+      className="dropdown__item"
+      onSelect={(
+        event: Event & {
+          currentTarget?:
+            | null
+            | (Event["currentTarget"] & { classList?: DOMTokenList });
+        }
+      ) => {
+        event.currentTarget?.classList?.add("press");
+        event.preventDefault();
       }}
-      className="w-40 select-none rounded px-2 py-1.5 text-gray-700 data-[highlighted]:bg-sky-400 data-[highlighted]:text-white data-[highlighted]:focus:outline-none"
-      asChild
-    >
-      <motion.div animate={controls}>{children}</motion.div>
-    </DropdownMenu.Item>
+      onAnimationEnd={(event) => {
+        event.currentTarget.classList.remove("press");
+        props.onSelect?.();
+      }}
+    />
   );
-}
-
-const sleep = (s: number) =>
-  new Promise((resolve) => setTimeout(resolve, s * 1000));
+};
